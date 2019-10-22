a98047b362f2170d50c98c8e6886388aa5c15ad7
Antonio <Memphis2Coder@gmail.com>
2019-10-22 00:22:21 -0500

    Updates

---
+++ precache-manifest.a06eceeb98a9b10461a99c860c711f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2dcaa2770e097b78910e8a3e9d5a6e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "5a55e20609542403c0d6",
+    "url": "/react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7c95b7eeab969cb2bbcd",
+    "url": "/react-app/static/js/2.c804eb51.chunk.js"
+  },
+  {
+    "revision": "5a55e20609542403c0d6",
+    "url": "/react-app/static/js/main.393c7056.chunk.js"
+  },
+  {
+    "revision": "eb173cd155f1f211940b",
+    "url": "/react-app/static/js/runtime-main.9d9e6ae3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-app/static/media/logo.25bf045c.svg"
+  }
+]);
